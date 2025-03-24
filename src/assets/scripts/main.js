40b61290061010ae6f5a0a7f555a16dcbd6b166a
Alfonso J <alfonsojfp@gmail.com>
2025-03-24 20:57:57 +0100

    Pagina ok- falta footer

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -32,7 +32,7 @@ const recipes = [
 ];
 
 const container = document.getElementById('recipe-container');
-
+//https://programadorwebvalencia.com/boton-para-volver-arriba-animado-o-scroll-suave/ volver arruiba
 recipes.forEach(recipe => {
   const recipeDiv = document.createElement('div');
   recipeDiv.innerHTML = `<h3>${recipe.name}</h3><ul class='list-ingredientes'>${recipe.ingredients.map(ing => `<li>${ing}</li>`).join('')}</ul>`;
